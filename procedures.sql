c968abd5473599c7dca7f61699076d5e747a531a
Alexandra2899 <59295650+Alexandra2899@users.noreply.github.com>
2020-01-02 22:59:09 +0000

    Update procedures.sql

--- procedures.sql
+++ procedures.sql
@@ -11,19 +11,32 @@ USE umclinic;
 #drop procedure p_finalizarConsulta;
 #drop procedure p_addObservations;
 
-#drop procedure p_adicionarConsulta;
 DELIMITER //
-CREATE PROCEDURE p_adicionarConsulta (IN idDoctor INT(11), idAthlete INT(11), obs TEXT, p DECIMAL(7,2), dateAppointment Datetime, f TINYINT(4))
+CREATE PROCEDURE p_completedAppointments()
 BEGIN
-INSERT INTO appointment (idDoctor, idAthlete, observations, price, dateAppointment, finished)
-VALUES (idDoctor, idAthlete, obs, p, dateAppointment, f);
+Select atl.nameAthlete as Athlete, d.nameDoctor as Doctor, a.dateAppointment, a.observations, a.price
+from Appointment a inner join athlete atl on a.idAthlete = atl.idAthlete
+inner join doctor d on d.idDoctor = a.idDoctor
+where finished = 1;
+END //
+DELIMITER ;
+																    
+#drop procedure p_scheduledAppointmentsBetweenDates;
+DELIMITER //
+CREATE PROCEDURE p_scheduledAppointmentsBetweenDates (IN date1 Datetime, date2 Datetime)
+BEGIN
+Select atl.nameAthlete as Athlete, d.nameDoctor as Doctor, a.dateAppointment, a.observations, a.price, a.finished
+from Appointment a inner join athlete atl on a.idAthlete = atl.idAthlete
+inner join doctor d on d.idDoctor = a.idDoctor
+where a.dateAppointment>=date1
+and a.dateAppointment <= date2
+and finished = 0;
 END //
 DELIMITER ;
 
-
-#drop procedure p_alterarHorarioConsulta;
+#drop procedure p_alterDateAppointment;
 DELIMITER //
-CREATE PROCEDURE p_alterarHorarioConsulta (IN newDate Datetime, original Datetime, idD INT(11), idA INT(11))
+CREATE PROCEDURE p_alterDateAppointment (IN newDate Datetime, original Datetime, idD INT(11), idA INT(11))
 BEGIN
 if (f_diasAteConsulta(original)>0 
 	and f_diasAteConsulta(newDate)>0 
@@ -33,22 +46,23 @@ END IF;
 END //
 DELIMITER ;
 
-
-#drop procedure p_listarConsultasEntreDatas;
+#drop procedure p_alterStateAppointment;
 DELIMITER //
-CREATE PROCEDURE p_listarConsultasEntreDatas (IN date1 Datetime, date2 Datetime)
+CREATE PROCEDURE p_alterStateAppointment (IN d Datetime, idD INT(11), idA INT(11))
 BEGIN
-Select atl.nameAthlete as Athlete, d.nameDoctor as Doctor, a.dateAppointment, a.observations, a.price, a.finished
-from Appointment a inner join athlete atl on a.idAthlete = atl.idAthlete
-inner join doctor d on d.idDoctor = a.idDoctor
-where a.dateAppointment>=date1
-and a.dateAppointment <= date2;
+UPDATE appointment
+SET finished = 1
+where idAthlete = idA
+and idDoctor = idD
+and dateAppointment = d;
 END //
 DELIMITER ;
+									      
 
-#drop procedure p_listarRealizadasEntreDatas;
+
+#drop procedure p_completedAppointmentsFromDate;
 DELIMITER //
-CREATE PROCEDURE p_listarRealizadasEntreDatas (IN date1 Datetime)
+CREATE PROCEDURE p_completedAppointmentsFromDate (IN date1 Datetime)
 BEGIN
 Select atl.nameAthlete as Athlete, d.nameDoctor as Doctor, a.dateAppointment, a.observations, a.price
 from Appointment a inner join athlete atl on a.idAthlete = atl.idAthlete
@@ -57,22 +71,60 @@ where a.dateAppointment<=date1
 and a.finished = 1;
 END //
 DELIMITER ;
+									      
 
 
-#drop procedure p_listarConsultasAtleta;
+
 DELIMITER //
-CREATE PROCEDURE p_listarConsultasAtleta (IN idA INT(11))
+CREATE PROCEDURE p_scheduleAppointmentsAthlete (IN idA INT(11))
 BEGIN
 Select atl.nameAthlete as Athlete, d.nameDoctor as Doctor, a.dateAppointment, a.observations, a.price, a.finished
 from Appointment a inner join athlete atl on a.idAthlete = atl.idAthlete
-inner join doctor d on d.idDoctor = a.idDoctor;
+inner join doctor d on d.idDoctor = a.idDoctor
+where finished = 0
+and a.idAthlete = idA;
+END //
+DELIMITER ;
+
+							   
+DELIMITER //
+CREATE PROCEDURE p_appointmentsCompletedAthlete (IN idA INT(11))
+BEGIN
+Select atl.nameAthlete as Athlete, d.nameDoctor as Doctor, a.dateAppointment, a.observations, a.price, a.finished
+from Appointment a inner join athlete atl on a.idAthlete = atl.idAthlete
+inner join doctor d on d.idDoctor = a.idDoctor
+where finished = 1
+and a.idAthlete = idA;
+END //
+DELIMITER ;
+							    
+DELIMITER //
+CREATE PROCEDURE p_appointmentsCompletedDoctor (IN idD INT(11))
+BEGIN
+Select atl.nameAthlete as Athlete, d.nameDoctor as Doctor, a.dateAppointment, a.observations, a.price, a.finished
+from Appointment a inner join athlete atl on a.idAthlete = atl.idAthlete
+inner join doctor d on d.idDoctor = a.idDoctor
+where finished = 1
+and a.idDoctor = idD;
 END //
 DELIMITER ;
 
 
-#drop procedure p_listarMarcadasEntreDatas;
 DELIMITER //
-CREATE PROCEDURE p_listarMarcadasEntreDatas (IN date1 Datetime)
+CREATE PROCEDURE p_scheduleAppointmentsDoctor (IN idD INT(11))
+BEGIN
+Select atl.nameAthlete as Athlete, d.nameDoctor as Doctor, a.dateAppointment, a.observations, a.price, a.finished
+from Appointment a inner join athlete atl on a.idAthlete = atl.idAthlete
+inner join doctor d on d.idDoctor = a.idDoctor
+where finished = 0
+and a.idDoctor = idD;
+END //
+DELIMITER ;							    
+
+
+#drop procedure p_scheduledAppointmentsAfterDate;
+DELIMITER //
+CREATE PROCEDURE p_scheduledAppointmentsAfterDate (IN date1 Datetime)
 BEGIN
 Select atl.nameAthlete as Athlete, d.nameDoctor as Doctor, a.dateAppointment, a.observations, a.price
 from Appointment a inner join athlete atl on a.idAthlete = atl.idAthlete
@@ -82,21 +134,12 @@ and a.finished = 0;
 END //
 DELIMITER ;
 
-#drop procedure p_eliminarConsulta;
-DELIMITER //
-CREATE PROCEDURE p_eliminarConsulta (IN idD INT(11), idA INT(11), d Datetime)
-BEGIN
-DELETE from appointment a 
-where a.idDoctor=idD
-and a.idAthlete=idA
-and a.dateAppointment = d;
-END //
-DELIMITER ;
 
 
-#drop procedure p_alterarPesoAtleta;
+
+#drop procedure p_alterWeightAthlete;
 DELIMITER //
-CREATE PROCEDURE p_alterarPesoAtleta (IN peso decimal(4,1), id INT(11))
+CREATE PROCEDURE p_alterWeightAthlete (IN peso decimal(4,1), id INT(11))
 BEGIN
 UPDATE athlete 
 SET weight = peso
@@ -106,9 +149,9 @@ DELIMITER ;
 
 
 
-#drop procedure p_finalizarConsulta;
+#drop procedure p_alterStateAppointment;
 DELIMITER //
-CREATE PROCEDURE p_finalizarConsulta (IN d Datetime, idD INT(11), idA INT(11))
+CREATE PROCEDURE p_alterStateAppointment (IN d Datetime, idD INT(11), idA INT(11))
 BEGIN
 UPDATE appointment
 SET finished = 1
@@ -131,7 +174,7 @@ END //
 DELIMITER ;
 
 DELIMITER //
-CREATE PROCEDURE p_medicosNaoConsultaAtletasCidade (IN cityAthlete varchar(30))
+CREATE PROCEDURE p_doctorsNotAthleteCity (IN cityAthlete varchar(30))
 BEGIN
 Select distinct d.nameDoctor as Name
 from appointment a  
@@ -142,9 +185,19 @@ where z.city not in (cityAthlete);
 END //
 DELIMITER ;
 
+drop procedure p_alterZipcodeAthlete;
+DELIMITER //
+CREATE PROCEDURE p_alterZipcodeAthlete (IN codigoPostal VARCHAR(10), id INT(11))
+BEGIN
+UPDATE athlete 
+SET idZipcode = codigoPostal
+where idAthlete = id;
+END //
+DELIMITER ;
+									    
 
 DELIMITER //
-CREATE PROCEDURE p_atletasSoConsultadosUmaEspecialidade ()
+CREATE PROCEDURE p_athletesOneExpertise ()
 BEGIN
 Select atl.nameAthlete as Name, e.designation as Expertise
 from appointment a 
@@ -158,7 +211,7 @@ DELIMITER ;
 
 
 DELIMITER //
-CREATE PROCEDURE p_especialidadeComMaisConsultas ()
+CREATE PROCEDURE p_expertiseMoreAppointments ()
 BEGIN
 Select e.designation as Name, count(*) as NumberOfAppointments
 from appointment a 
@@ -170,7 +223,7 @@ END //
 DELIMITER ;
 
 DELIMITER //
-CREATE PROCEDURE p_categoriasComMaisConsultas ()
+CREATE PROCEDURE p_categoryMoreAppointments ()
 BEGIN
 Select c.nameCategory as Name, count(*) as NumberOfAppointments
 from appointment a 
@@ -183,7 +236,7 @@ DELIMITER ;
 
 
 DELIMITER //
-CREATE PROCEDURE p_modalidadesComMaisConsultas ()
+CREATE PROCEDURE p_modalityMoreAppointments ()
 BEGIN
 Select m.nameModality as Name, count(*) as NumberOfAppointments
 from appointment a 
@@ -196,7 +249,7 @@ DELIMITER ;
 
 #ainda falta testar esta
 DELIMITER //
-CREATE PROCEDURE p_clubesComMaisConsultas()
+CREATE PROCEDURE p_clubMoreAppointments()
 BEGIN
 Select c.nameClub as Name, count(*) as NumberOfAppointments
 from appointment a 
@@ -209,7 +262,7 @@ DELIMITER ;
 
 #ainda falta testar esta
 DELIMITER //
-CREATE PROCEDURE p_atletasComMaisConsultas ()
+CREATE PROCEDURE p_athleteMoreAppointments ()
 BEGIN
 Select atl.nameAthlete as Name, count(*) as NumberOfAppointments
 from appointment a, athlete atl
@@ -221,7 +274,7 @@ DELIMITER ;
 
 #ainda falta testar esta
 DELIMITER //
-CREATE PROCEDURE p_medicoComMaisConsultas()
+CREATE PROCEDURE p_moreAppointmentsDoctor()
 BEGIN
 Select d.nameDoctor as Name, count(*) as NumberOfAppointments
 from appointment a, doctor d
@@ -234,7 +287,7 @@ DELIMITER ;
 
 #ainda falta testar esta
 DELIMITER //
-CREATE PROCEDURE p_consultasPorMedico()
+CREATE PROCEDURE p_appointmentsByDoctor()
 BEGIN
 Select d.nameDoctor as Name, count(*) as NumberOfAppointments
 from appointment a, doctor d
@@ -246,7 +299,7 @@ DELIMITER ;
 
 #ainda falta testar esta
 DELIMITER //
-CREATE PROCEDURE p_mediaPeso()
+CREATE PROCEDURE p_AverageWeight()
 BEGIN
 Select avg(a.weight) as "Peso médio" 
 from athlete a;
@@ -256,7 +309,7 @@ DELIMITER ;
 
 #ainda falta testar esta
 DELIMITER //
-CREATE PROCEDURE p_medicosPorEspecialidade ()
+CREATE PROCEDURE p_doctorsByExpertise ()
 BEGIN
 Select e.designation as Expertise, count(*) as NumberOfDoctors
 from doctor d, expertise e
@@ -267,20 +320,24 @@ END //
 DELIMITER ;
 
 DELIMITER //
-CREATE PROCEDURE p_totalFaturado ()
+CREATE PROCEDURE p_totalProfit ()
 BEGIN
 Select sum(a.price) as montante
 from appointment a
 where finished = 1;
 END //
 DELIMITER ;
-
+		
 DELIMITER //
-CREATE PROCEDURE p_consultasPassadas ()
+CREATE PROCEDURE p_athletesOneExpertise ()
 BEGIN
-Select atl.nameAthlete as Athlete, d.nameDoctor as Doctor, a.dateAppointment, a.observations, a.price
-from Appointment a inner join athlete atl on a.idAthlete = atl.idAthlete
-inner join doctor d on d.idDoctor = a.idDoctor
-where finished = 1;
+Select atl.nameAthlete as Name, e.designation as Expertise
+from appointment a 
+INNER JOIN athlete atl on atl.idAthlete= a.idAthlete
+INNER JOIN doctor d on  d.idDoctor = a.idDoctor
+INNER JOIN expertise e on d.idExpertise = e.idExpertise
+group by a.idAthlete
+having count(*) = 1;
 END //
 DELIMITER ;
+
