99309d9c23d880b4918fe911ebcd504ce994c9d3
RodrigoProjects <RodrigoProjects@users.noreply.github.com>
2020-01-02 16:04:06 +0000

    Users com permissões exatas - Rodrigo

--- users.sql
+++ users.sql
@@ -1,18 +1,14 @@
-#DA Privileges
-CREATE USER 'DA' IDENTIFIED BY 'password';
-GRANT ALL ON umclinic TO 'custom'@'localhost';
+-- Athlete Privileges
+CREATE USER Athlete WITH PASSWORD = '?';
+GRANT SELECT ON umclinic.appointment TO Athlete;
 
-#Athlete Privileges
-CREATE USER 'Athlete' IDENTIFIED BY 'password';
-GRANT UPDATE ON umclinic.athlete TO 'custom'@'host47.example.com';
-
-#Receptionist Privileges
-CREATE USER 'Receptionist' IDENTIFIED BY 'password';
-GRANT SELECT,INSERT,UPDATE,DELETE ON umclinic.appointment TO 'custom'@'%.example.com';
+-- Receptionist Privileges
+CREATE USER Receptionist WITH PASSWORD = '?';
+GRANT SELECT,INSERT,UPDATE,DELETE ON umclinic.appointment TO Receptionist;
   
-#Doctor Privileges  
-CREATE USER 'Doctor'  IDENTIFIED BY 'password';
-GRANT UPDATE ON umclinic.doctor TO 'custom'@'%.example.com';
+-- Doctor Privileges  
+CREATE USER Doctor  WITH PASSWORD = '?';
+GRANT SELECT ON umclinic.appointment TO Doctor;
   
   
   
