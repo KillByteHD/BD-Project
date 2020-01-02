e77ccc9b7ced9002d52758f3585667bde022a399
Alexandra2899 <59295650+Alexandra2899@users.noreply.github.com>
2020-01-02 21:42:33 +0000

    Update views.sql

--- views.sql
+++ views.sql
@@ -1,16 +1,16 @@
-select * from v_consultasAgendadas;
+select * from v_scheduledAppointments;
 
-Create view v_consultasAgendadas as 
+Create view v_scheduledAppointments as 
 Select atl.nameAthlete as Athlete, d.nameDoctor as Doctor, a.dateAppointment as date, f_diasAteConsulta(a.dateAppointment) as "days to appointment"
 from Appointment a inner join athlete atl on a.idAthlete = atl.idAthlete
 inner join doctor d on d.idDoctor = a.idDoctor
 where finished = 0;
 
-drop view v_consultasAgendadasSemana;
+drop view v_scheduledAppointmentsWeek;
 
-select * from v_consultasAgendadasSemana;
+select * from v_scheduledAppointmentsWeek;
 
-create view v_consultasAgendadasSemana as
+create view v_scheduledAppointmentsWeek as
 Select atl.nameAthlete as Athlete, d.nameDoctor as Doctor, a.dateAppointment as date, f_diasAteConsulta(a.dateAppointment) as "days to appointment"
 from Appointment a inner join athlete atl on a.idAthlete = atl.idAthlete
 inner join doctor d on d.idDoctor = a.idDoctor
